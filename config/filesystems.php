cceae374f2cd94eeaeb693d73d6388f951015d5d
wholeomarfaruk <wholeomarfaruk@gmail.com>
2026-01-10 23:11:38 +0600

    update config

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,10 +38,11 @@ return [
             'report' => false,
         ],
 
+
         'public' => [
             'driver' => 'local',
-            'root' => public_path(),
-            'url' =>    env('APP_URL') . '/storage',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
