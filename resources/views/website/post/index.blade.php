a15ebaa125f199f87747b5dedcb31c6b32ee0e52
wholeomarfaruk <wholeomarfaruk@gmail.com>
2026-01-10 22:00:03 +0600

    update

--- resources/views/website/post/index.blade.php
+++ resources/views/website/post/index.blade.php
@@ -7,7 +7,7 @@
 @section('meta_twitter_title', $post->title)
 @section('meta_twitter_description', Str::limit(strip_tags($post->excerpt), 160))
 @section('meta_twitter_image', $post->featured_image)
-@section('meta_canonical', route('post.show', ['category' => $post->category->slug, 'slug' => $post->slug]))
+@section('meta_canonical', $post->PostUrl)
 
 @section('content')
     @livewire('post-detail', ['id' => $post->id],key($post->id))
