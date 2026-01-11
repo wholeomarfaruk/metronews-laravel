b352ae9c5a2c9f11f8325b46bb1c8a70c6489746
wholeomarfaruk <wholeomarfaruk@gmail.com>
2026-01-11 07:50:52 +0600

    add

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -261,7 +261,7 @@
                         @enderror
 
                         @if ($featured_image)
-                            @dd($featured_image->realPath);
+                            @dd($featured_image->realPath());
                             <div class="mt-2">
                                 <label class="block text-xs text-gray-500 dark:text-gray-400 mb-1">Preview:</label>
                                 <img src="{{ $featured_image->temporaryUrl() }}" alt="Image Preview"
