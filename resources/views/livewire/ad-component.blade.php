7c9ba590da66a131d5cabd0dbcd1bedce9f0fd39
wholeomarfaruk <wholeomarfaruk@gmail.com>
2026-01-17 00:23:46 +0600

    update

--- resources/views/livewire/ad-component.blade.php
+++ resources/views/livewire/ad-component.blade.php
@@ -2,7 +2,7 @@
 
 
 <div>
-    @if ($ad)
+    @if ($ad && $ad->status == 1 && $ad->expire_at > now())
     <div class="ad d-flex justify-content-center align-items-center mb-2">
         <a class="d-block w-100" href="{{ $ad->link }}" target="_blank">
     <img src="{{ $ad->image }}" class="w-100" alt="">
