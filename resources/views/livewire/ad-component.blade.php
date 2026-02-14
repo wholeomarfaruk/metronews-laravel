5001e262264de25aba94fd8ea8c98d597853519a
wholeomarfaruk <wholeomarfaruk@gmail.com>
2026-02-15 00:47:41 +0600

    ad

--- resources/views/livewire/ad-component.blade.php
+++ resources/views/livewire/ad-component.blade.php
@@ -1,6 +1,6 @@
 <div class="">
     @if ($ad && $ad->status == 1 && $ad->expire_at > now())
-        <a class="ad d-flex justify-content-center align-items-center mb-2 py-1" href="{{ $ad->link }}" target="_blank" >
+        <a class="ad d-flex justify-content-center align-items-center py-1" href="{{ $ad->link }}" target="_blank" >
             <img src="{{ $ad->image }}" style="max-height: {{ $height ?? 'inherit' }}"  alt="">
         </a>
     @endif
