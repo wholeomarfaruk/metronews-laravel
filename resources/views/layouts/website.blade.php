fbfec66c962f66185709c9fd7750811956b4c324
wholeomarfaruk <wholeomarfaruk@gmail.com>
2026-03-10 02:27:21 +0600

    update

--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -146,8 +146,8 @@
         </div>
 
         <div id="menubar" class="menu_bar   pt-1 ">
-            <nav class="px-2 navbar  navbar-expand-lg navbar-light p-0">
-                <div class="container-fluid wrapper menu-area">
+            <nav class=" navbar  navbar-expand-lg navbar-light p-0">
+                <div class="container-fluid wrapper menu-area px-2">
                     <!-- Brand -->
                     <div class="logo" style="max-width: 75px !important;">
                         <a href="/">
